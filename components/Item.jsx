f7397f4a04c47d11cff0f5372f64389c135d0c20
alphageekdom <97497295+alphageekdom@users.noreply.github.com>
2024-05-23 23:52:19 -0700

    fixed item bug

--- components/Item.jsx
+++ components/Item.jsx
@@ -1,62 +1,40 @@
 'use client';
 
 import React from 'react';
-import { useState, useEffect, useRef, useCallback } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { useParams, usePathname } from 'next/navigation';
 
-import Breadcrumbs from './common/Breadcrumbs';
+import { useCart } from '@/context/CartContext';
+
 import Spinner from './common/Spinner';
 import ErrorMessage from './common/ErrorMessage';
 import ItemDetails from './menu/itemDetails';
 
 const Item = ({ product }) => {
-  const [item, setItem] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const params = useParams();
-
-  const pathname = usePathname();
-
+  const { loading, error } = useCart();
   const mountCount = useRef(0);
-
-  const fetchItem = useCallback(async () => {
-    try {
-      const response = await fetch(`/api/product/${params?.item}`);
-
-      if (!response.ok) {
-        throw new Error('Failed to fetch item');
-      }
-      const itemData = await response.json();
-      setItem(itemData);
-      setLoading(false);
-    } catch (error) {
-      setError(error.message);
-      setLoading(false);
-    }
-  }, [params?.id]);
 
   useEffect(() => {
     console.log(`Component mounted. Mount count: ${mountCount.current}`);
     mountCount.current++;
-    fetchItem();
-  }, [fetchItem]);
+  }, []);
 
-  // if (!item) {
-  //   return <div>Item not found</div>;
-  // }
+  if (!product) {
+    return <div>Item not found</div>;
+  }
 
   if (loading) return <Spinner />;
 
-  if (error) return <ErrorMessage message={error} />;
+  if (error) return <ErrorMessage error={error.message} />;
 
   return (
     <div>
       <h1 className='text-4xl font-bold text-center text-black mb-10'>
-        {item.name}
+        {product?.name}
       </h1>
       <div className='custom-shadow'>
-        <ItemDetails item={item} />
+        <ItemDetails product={product} />
       </div>
     </div>
   );
--- components/menu/itemDetails.jsx
+++ components/menu/itemDetails.jsx
@@ -8,9 +8,9 @@ import toast from 'react-hot-toast';
 import Spinner from '../common/Spinner';
 import ErrorMessage from '../common/ErrorMessage';
 
-const ItemDetails = ({ item }) => {
+const ItemDetails = ({ product }) => {
   const { addToCart, loading, error } = useCart();
-  const [selectedSize, setSelectedSize] = useState(item?.sizes[0]);
+  const [selectedSize, setSelectedSize] = useState(product?.sizes[0]);
   const [quantity, setQuantity] = useState(1);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
@@ -20,37 +20,45 @@ const ItemDetails = ({ item }) => {
 
   const handleAddToCart = async () => {
     const cartItem = {
-      name: item?.name,
+      name: product?.name,
       size: selectedSize,
-      type: item?.type,
-      itemId: item?.id,
+      type: product?.type,
+      itemId: product?.id,
       quantity,
-      price: parseFloat(item?.prices[selectedSize]),
-      imageUrl: item?.images[0],
+      price: parseFloat(product?.prices[selectedSize]),
+      imageUrl: product?.images[0],
     };
 
     addToCart(cartItem);
     toast.success('Item added to cart!');
   };
 
+  console.log(product);
+
+  if (loading) return <Spinner />;
+
+  if (error) return <ErrorMessage />;
+
   return (
     <div className='mx-auto p-4 relative'>
       <div className='flex flex-col md:flex-row items-center justify-center'>
         <div className='md:w-1/2 md:pr-8 relative'>
           <Image
-            src={item?.images[currentImageIndex]}
-            alt={item?.name}
+            src={product?.images[currentImageIndex]}
+            alt={product?.name}
             width={300}
             height={300}
             className='h-64 w-64 md:w-full md:h-[500px] object-cover md:rounded-lg'
             loading='lazy'
           />
-          {item?.images?.length > 1 && (
+          {product?.images?.length > 1 && (
             <div className='absolute top-1/2 transform -translate-y-1/2 flex items-center w-full'>
               <button
                 onClick={() =>
                   setCurrentImageIndex((prevIndex) =>
-                    prevIndex === 0 ? item?.images?.length - 1 : prevIndex - 1
+                    prevIndex === 0
+                      ? product?.images?.length - 1
+                      : prevIndex - 1
                   )
                 }
                 className='text-white px-2 py-1 rounded-full flex items-center justify-center ml-0 md:ml-2 left-0 absolute transition duration-300 ease-in-out opacity-50 hover:opacity-100'
@@ -61,7 +69,9 @@ const ItemDetails = ({ item }) => {
               <button
                 onClick={() =>
                   setCurrentImageIndex((prevIndex) =>
-                    prevIndex === item?.images?.length - 1 ? 0 : prevIndex + 1
+                    prevIndex === product?.images?.length - 1
+                      ? 0
+                      : prevIndex + 1
                   )
                 }
                 className='text-white px-2 py-1 rounded-full flex items-center justify-center right-0 mr-0 md:mr-12 absolute transition duration-300 ease-in-out opacity-50 hover:opacity-100'
@@ -74,18 +84,18 @@ const ItemDetails = ({ item }) => {
         </div>
 
         <div className='md:w-1/2 mt-4 md:mt-0'>
-          <p className='text-gray-600 text-lg mt-2'>{item?.type}</p>
+          <p className='text-gray-600 text-lg mt-2'>{product?.type}</p>
           <div className='flex items-center mt-4'>
-            {Object?.entries(item?.prices).map(([size, price]) => (
+            {Object?.entries(product?.prices).map(([size, price]) => (
               <p key={size} className='mr-2'>
                 {size}: ${price?.toFixed(2)}
               </p>
             ))}
           </div>
           <p className='text-gray-700 mt-2'>
-            Availability: {item?.availability}
+            Availability: {product?.availability}
           </p>
-          <p className='text-gray-700 mt-4'>{item?.description}</p>
+          <p className='text-gray-700 mt-4'>{product?.description}</p>
 
           <div className='mt-4'>
             <label htmlFor='size' className='text-gray-700'>
@@ -97,7 +107,7 @@ const ItemDetails = ({ item }) => {
               onChange={handleSizeChange}
               className='ml-2 px-2 py-1 border border-gray-300 rounded mb-4'
             >
-              {item?.sizes?.map((size) => (
+              {product?.sizes?.map((size) => (
                 <option key={size} value={size}>
                   {size}
                 </option>
